0041ccf89f8c808ef149886009536ccbc8b1c95a
fidias.rocca <fidias.rocca@ebanx.com>
2021-05-12 20:17:28 -0300

    test

--- src/test.java
+++ src/test.java
@@ -1,18 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        final int LIN = 3;
-        final int COL = 4;
 
-        int [][] mat = new int[LIN][COL];
-        int valor;
 
-        for (int i = 0; i < LIN; i++){
-            for (int j = 0; j < COL; j++){
-                valor = i + j;
-                mat[i][j] = valor;
-                System.out.println(mat[i][j] + "");
-            }
-            System.out.println();
-        }
     }
 }
